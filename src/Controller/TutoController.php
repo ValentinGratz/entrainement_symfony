8bb302d747387d8afee864cbf8756db80261c84c
Valentin <valentin.gratz@orange.fr>
2020-12-14 13:13:27 +0100

    Initial commit

---
+++ src/Controller/TutoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TutoController extends AbstractController
+{
+    /**
+     * @Route("/tuto", name="tuto")
+     */
+    public function index(): Response
+    {
+        return $this->render('tuto/index.html.twig', [
+            'controller_name' => 'TutoController',
+        ]);
+    }
+}
